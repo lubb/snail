4af7afc84f7e89bb57fe71b652ff8462796b0ee4
lubingbing <414107871@qq.com>
2019-09-05 18:04:42 +0800

    修改打包后版本缓存问题

--- snail-biz-web/src/main/webapp-src/vue.config.js
+++ snail-biz-web/src/main/webapp-src/vue.config.js
@@ -1,3 +1,4 @@
+const Timestamp = new Date().getTime();
 module.exports = {
     publicPath: './',
     outputDir:'../static-resources/static',
@@ -5,5 +6,11 @@ module.exports = {
     productionSourceMap: false,
     devServer:{
        proxy:'http://localhost:8888/'
-    }
+    },
+    configureWebpack: { // webpack 配置
+        output: { // 输出重构  打包编译后的 文件名称  【模块名称.版本号.时间戳】
+            filename: `static/js/[name].${process.env.NODE_ENV}.${Timestamp}.js`,
+            chunkFilename: `static/js/[name].${process.env.NODE_ENV}.${Timestamp}.js`
+        },
+    },
 }
